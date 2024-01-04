7d42809e43135a4f4e3a8849e0cd7adec4b1758c
Daniel Weiss <DanWes99@gmail.com>
2024-01-04 01:09:04 -0500

    Added 4 more ships for the player to place, for a total of 5. Changed condition checking if player's ships have been sunk to accomodate all ships.

--- main.py
+++ main.py
@@ -63,50 +63,85 @@ def display_board(board):
   for row in board:
     print(" ".join(row))
 
+# Player Introduction
+print("""
+      Welcome to Battleship!
+      Good news! The government allotted us more money at the expense of useless housing and road-fixing projects, so we have FIVE ships now!
+      This will make us a VERY powerful navy that will put FEAR into the enemy computer.
+      At the moment, our construction team isn't the brightest, so if you build two ships at the same place... well...
+      Let's just say they won't bat an eye when the two ships merge into one. Try to avoid doing that, as smart as it seems.
+      """)
 
 # Set up boards
 player_board = [['O' for _ in range(10)] for _ in range(10)]
 
 # Creating bow coordinates and 0-indexed coordinates of player's ship
-print("Welcome to Battleship!")
-x_cord = int(input("You get 1 battleship. Yeah... budget's tight this year. Where do you want her? Bow x-coordinate (1-10): "))
-y_cord = int(input("Bow y-coordinate (1-10): "))
-x = x_cord-1
-y = y_cord-1
 
-# Create player battleship and update board
-player_battleship = Ship(ship_type="battleship", size=5)
+# Patrol Boat
+player_patrol_boat = Ship(ship_type="patrol boat", size=1)
+x = int(input(f"You get a Patrol Boat. Where do you want her? X-coordinate (1-10): ")) - 1
+y = int(input("Bow y-coordinate (1-10): ")) - 1
+player_patrol_boat.place_ship(x, y)
+
+# Destroyer
+player_destroyer = Ship(ship_type="destroyer", size=2)
+x = int(input(f"You get a Destroyer. Where do you want her? X-coordinate (1-10): ")) - 1
+y = int(input("Bow y-coordinate (1-10): ")) - 1
+player_destroyer.place_ship(x, y)
+
+# Submarine
+player_submarine = Ship(ship_type="submarine", size=3)
+x = int(input(f"You get a Submarine. Where do you want her? X-coordinate (1-10): ")) - 1
+y = int(input("Bow y-coordinate (1-10): ")) - 1
+player_submarine.place_ship(x, y)
+
+# Battleship
+player_battleship = Ship(ship_type="battleship", size=4)
+x = int(input(f"You get a Battleship! Where do you want her? X-coordinate (1-10): ")) - 1
+y = int(input("Bow y-coordinate (1-10): ")) - 1
 player_battleship.place_ship(x, y)
 
+# Carrier
+player_carrier = Ship(ship_type="carrier", size=5)
+x = int(input(f"You get an Aircraft Carrier. Where do you want her? X-coordinate (1-10): ")) - 1
+y = int(input("Bow y-coordinate (1-10): ")) - 1
+player_carrier.place_ship(x, y)
+
 print("----------------------------------------------------")
-print(f"These are your battleship's coordinates (they are 0-indexed):\n{player_battleship.coords}")
-print("Don't know what 0-indexed means? Don't worry about it :)")
 print("Player board:")
 display_board(player_board)
 
-print("""\nThis is your part of the sea. For now. The S's represent your ship.
-Now, in a few seconds, watch in utter helplessness as the enemy computer barrages your
-part of the sea with completely random strikes that you can't do anything about,
-until your stupid new ship has sunk. The computer wants you OUT of this waterspace.
-One day though... you may exact your sweet, sweet revenge.
-For now, I don't know. Maybe duck?\n""")
+print("""\n
+      This is your part of the sea. For now. The S's represent your ships.
+      But...
+      I got some bad news.
+      We got so carried away with spending while building the ships that...
+      Yeah... we can't afford to buy any ammunition for the ships already placed in the water.
+      Unfortunate.
+      I'm gonna try to sort this out with the admiral without getting honorably discharged.
+      In the meantime, we've got reports of the enemy advancing on your position.
+      They will be firing relentlessly pretty soon, so... I don't know. Maybe duck?\n
+      """)
 
 user_input = input("Okay. You ready? Type 'ready' or 'not': ")
-print("Doesn't matter! Here comes the AI shells!\n")
+print("Doesn't matter! Here come the artillery shells!\n")
 time.sleep(2)
 
 plays = 0
-while player_battleship.coords:
+all_ships_sunk = False
+
+# Iterates through until 'all_ships_sunk' finds no more "S"'s on the board, meaning all player ships have been sunk
+while not all_ships_sunk:
   picking = True
   while picking:
     random_x_cord = random.randint(0, 9)
     random_y_cord = random.randint(0, 9)
     current_pick = player_board[random_y_cord][random_x_cord]
-    # loops through to make sure AI doesn't choose already shot sector
+    # loops through to make sure computer doesn't choose already shot sector
     if current_pick in ["O", "S"]:
       print(f"\n      {random_x_cord+1}, {random_y_cord+1}!")
 
-      # AI hits battleship
+      # Computer hits battleship
       if current_pick == "S":
 
         player_board[random_y_cord][random_x_cord] = "X"
@@ -114,22 +149,24 @@ while player_battleship.coords:
         display_board(player_board)
         time.sleep(1)
         print(f"\nYeah mf!! Hit! Back to shooting off my shots at random.")
-        player_battleship.coords.remove((random_x_cord, random_y_cord))
         time.sleep(1)
 
-      # AI misses
+      # Computer misses
       else:
         time.sleep(0.3)
         player_board[random_y_cord][random_x_cord] = "-"
         display_board(player_board)
 
-      # updates after each time AI shoots
+      # updates after each time computer shoots
       plays += 1
+      all_ships_sunk = all("S" not in row for row in player_board)
       picking = False
 
 print("Get SUNK mf!")
 
 if plays > 75:
-  print(f"\nIt took the AI {plays} turns to sink your battleship. What an idiot. Anyway, thanks for playing. Better luck next time.")
+  print(f"\nIt took the enemy computer {plays} turns to sink your battleship. What an idiot. Anyway, thanks for playing. Better luck next time.")
+elif plays < 20:
+  print(f"\nIt took the enemy computer {plays} turns to sink your battleship. Oh wow, what a lucky fella. Anyway, thanks for playing. Better luck next time.")
 else:
-  print(f"\nIt took the AI {plays} turns to sink your battleship. Thanks for playing. Better luck next time.")
+  print(f"\nIt took the enemy computer {plays} turns to sink your battleship. Thanks for playing. Better luck next time.")
